37a7203d463a6bbce826ce4b628b92eff804c908
tadamiura <tadamiura@gmail.com>
2021-12-12 02:14:03 +0100

    install naive ui & fonts

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './App.scss'
+import 'vfonts/Lato.css'
+import 'vfonts/FiraSans.css'
 
 createApp(App).use(store).use(router).mount('#app')
