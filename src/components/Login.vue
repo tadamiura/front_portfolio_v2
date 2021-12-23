5eb2af8e7b8f6cf326d3ee3941b79899a4c5e0bc
tadamiura <tadamiura@gmail.com>
2021-12-23 22:11:13 +0100

    created store, auth handler and message displaying

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,116 +1,108 @@
 <template>
-<n-card title="Se connecter" size="huge" hoverable=true>
-  <n-form 
-    :label-width="80"
-    :model="formData"
-    :rules="rules"
-    ref="formRef"
-  >
-    <n-form-item path="email" label="Adresse email">
-      <n-input
-        type="email"
-        v-model:value="formData.email"
-        size="large"
-        placeholder="Votre adresse email"
-      />
-     </n-form-item>
-     <n-form-item path="password" label="Mot de passe">
-      <n-input
-        type="password"
-        v-model:value="formData.password"
-        show-password-on="click"
-        size="large"
-        placeholder="Votre mot de passe"
-      />
-     </n-form-item>
-     <n-form-item>
-      <n-button
-        :loading="loading"
-        @click="handleValidateClick"
-        type="success" 
-        size="medium"
-       >
-        Valider
-      </n-button>
-     </n-form-item>
-  </n-form>
-</n-card>
+  <n-card title="Se connecter" size="huge" hoverable>
+    <n-form :label-width="80" :model="formData" :rules="rules" ref="formRef">
+      <n-form-item path="email" label="Adresse email">
+        <n-input
+          type="email"
+          v-model:value="formData.email"
+          size="large"
+          placeholder="Votre adresse email"
+        />
+      </n-form-item>
+      <n-form-item path="password" label="Mot de passe">
+        <n-input
+          type="password"
+          v-model:value="formData.password"
+          show-password-on="click"
+          size="large"
+          placeholder="Votre mot de passe"
+        />
+      </n-form-item>
+      <n-form-item>
+        <n-button
+          :loading="loading"
+          @click="handleValidateClick"
+          type="success"
+          size="medium"
+        >
+          Valider
+        </n-button>
+      </n-form-item>
+    </n-form>
+  </n-card>
 </template>
 
 <script>
-  import { defineComponent , ref } from 'vue'
-  import { NButton, NInput, NForm, NFormItem, NCard, useMessage } from 'naive-ui'
-  import axios from 'axios'
+import { defineComponent, ref } from "vue";
+import { NButton, NInput, NForm, NFormItem, NCard, useMessage } from "naive-ui";
+import router from "./../router/index";
+import { useStore } from "vuex";
 
-  export default defineComponent({
-    name: 'Login',
-    components: {
-      NButton,
-      NInput,
-      NForm,
-      NFormItem,
-      NCard
-    },
-    
-    setup() {
-      const formRef = ref(null)
-      const message = useMessage()
-      const formData = ref({
-        email: '',
-        password: ''
-      })
-      const isLoading = ref(false)
-      return {
-        formRef,
-        formData,
-        rules: {
-          email: {
-            required: true,
-            message: "Veuillez renseigner le nom d'utilisateur",
-            trigger: 'blur'
-          },
-          password: {
-            required: true,
-            message: "Veuillez renseigner le mot de passe",
-            trigger: 'blur'
-          }
+export default defineComponent({
+  name: "Login",
+  components: {
+    NButton,
+    NInput,
+    NForm,
+    NFormItem,
+    NCard,
+  },
+
+  setup() {
+    const formRef = ref(null);
+    const message = useMessage();
+    const formData = ref({
+      email: "",
+      password: "",
+    });
+    const store = useStore();
+    const loadingRef = ref(false)
+    return {
+      formRef,
+      formData,
+      rules: {
+        email: {
+          required: true,
+          message: "Veuillez renseigner le nom d'utilisateur",
+          trigger: "blur",
         },
-        loading: isLoading,
-        handleValidateClick (e, data) {
-        e.preventDefault()
-        isLoading.value = true
+        password: {
+          required: true,
+          message: "Veuillez renseigner le mot de passe",
+          trigger: "blur",
+        },
+      },
+      loading: loadingRef,
+      handleValidateClick(e) {
+        e.preventDefault();
+        loadingRef.value = true
         formRef.value.validate((errors) => {
           if (!errors) {
-            axios.post('http://localhost:3000/api/auth', formData.value)
-            .then(res => res.headers['x-access-token'])
-            .then( data => { 
-              localStorage.clear()
-              localStorage.setItem('x-access-token', data)
-              message.success('Connecté(e)')
-              isLoading.value = false
-            })
-            .catch(err => {
-              localStorage.removeItem('user-token') // if the request fails, remove any possible user token if possible
-              message.error("Le nom de l'utilisateur ou le mot de passe est incorrect")
-              isLoading.value = false
+            store.dispatch("trySignIn", formData.value).then(()=> {
+              if(store.getters.isLoggedIn) {
+                message.success('Connecté(e)')
+                loadingRef.value = false
+                router.push('/')
+              } else {
+                message.error("Le nom de l'utilisateur ou le mot de passe est incorrect")
+                loadingRef.value = false
+              }
             })
           } else {
-            console.log(errors)
-            message.error('Les champs requis ne sont pas tous complétés')
-            isLoading.value = false
+            console.log(errors);
+            message.error("Les champs requis ne sont pas tous complétés");
           }
-        })
-      }
-
-      }
-    }
-  })
+        });
+      },
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-  .n-card {
-    max-width: 30rem;
-    margin: 0 auto;
-    background-color: rgba(255, 255, 255, .88);
-  }
+.n-card {
+  max-width: 30rem;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.88);
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,71 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    data: {},
+        isLoading: false,
+        isLoggedIn: localStorage.getItem("x-access-token") ? true : false,
+        jwtToken: localStorage.getItem("x-access-token"),
+        errors: [],
+  },
+  getters: {
+    isLoading: state => state.isLoading,
+    isLoggedIn: state => state.isLoggedIn,
+    errors: state => state.errors,
+    currentUser: state => state.data,
+    jwtToken: state => state.jwtToken
+},
+  actions: {
+    async trySignIn(context, credentials) {
+      try {
+          context.commit("updateIsLoading", true)
+          const response = await axios.post(
+            'http://localhost:3000/api/auth', credentials
+            )
+          context.commit("signInSuccess", response.data)
+      } catch (err) {
+          console.log('trySygnIn catch', err)
+          context.commit("signError", err)
+      }
+    },
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    updateIsLoading(state, isLoading) {
+        state.isLoading = isLoading
+    },
+    signUpSuccess(state) {
+        state.isLoading = false
+        state.errors = []
+        state.isLoggedIn = true;
+    },
+    signError(state, errors) {
+        state.isLoading = false
+        state.errors = errors.response.data
+        state.isLoggedIn = false;
+        state.data = errors.response.user;
+        state.jwtToken = errors.response.jwtToken;
+        localStorage.removeItem("x-access-token");
+    },
+    signInSuccess(state, data) {
+        state.isLoading = false;
+        state.errors = [];
+        state.isLoggedIn = true;
+        state.data = data.user;
+        state.jwtToken = data.jwtToken;
+        localStorage.setItem("x-access-token", data.jwtToken);
+    },
+    signOut(state) {
+        state.jwtToken = null
+        state.data = null
+        state.isLoggedIn = false
+        localStorage.removeItem("x-access-token")
+    },
+    fetchCurrentUserSuccess(state, user) {
+        state.data = user
+        state.isLoading = false
+        state.isLoggedIn = true
+        state.errors = []
+    }
+  } 
 })
